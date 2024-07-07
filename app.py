eea1d061fd70effad532c12d233b2fff4c7cd2c6
Prithwijit22 <ghoshprithwijit39@gmail.com>
2024-07-07 21:10:57 +0530

    win_files

--- app.py
+++ app.py
@@ -96,12 +96,6 @@ st.download_button(
 )
 
 
-
-
-
-
-
-
 ## Checkbox
 agree = st.checkbox('I agree')
 
@@ -128,26 +122,85 @@ option = st.selectbox(
 
 
 ## Slider
+from datetime import datetime
 age = st.slider(
     'How old are you?',
     0,130,25
 )
-st.write('')
+if age == 29:
+    st.write('GilliDanda Kheloge???')
+else:
+    st.write('Tillu kalia')
+
+AGE = st.slider(
+    'How old are you?',
+    0,130,(25,60)
+)
+
+time = st.slider(
+    'How old are you?',
+    datetime(2021,1,1),datetime(2024,4,1),datetime(2022,2,1)
+)
+
+##Text input
+title = st.text_input('movie title','pk')
+st.write('the current movie title is : ',title)
 
 
 
+##Number input
+num = st.number_input('Enter your age',25)
+st.write('Your age is : ',num)
+
+##Date input
+date = st.date_input("Enter today's date",datetime(2019,7,6))
+st.write("Todays's date is : ",date)
+
+
+## Workung with images
+from PIL import Image
+image = Image.open('download.jpeg')
+st.image(image,caption='landscape')
+
+
+##working with vdo
+vdo_file = open('vdo.mp4','rb')
+vdo_bytes = vdo_file.read()
+st.video(vdo_bytes)
+
+
+##Side bar
+st.sidebar.selectbox(
+    'Choose the prefered way of contact',
+    ('Email','Gmail','Whatsapp')
+)
+
+with st.sidebar:
+    radio = st.radio('Choose yuor language of communication',
+    ('Bengali','Hindi','English'))
 
 
 
+## Columns
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.header('This is column 1')
+    st.title('col 1')
+    st.write('some text inside column 1')
+
+with col2:
+    st.header('This is column 2')
+    st.title('col 2')
+    st.write('some text inside column 2')
+
+with col3:
+    st.header('This is column 3')
+    st.title('col 3')
+    st.write('some text inside column 3')
 
 
+##Expander
+st.bar_chart({'data':[1,5,2,6,2,1]})
 
-
-
-
-
-
-
-
-
-
+with st.expander('Expand me to see some text'):
+    st.write('some text written inside expander')
